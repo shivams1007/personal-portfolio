9dfbd2c904f97152574af80e1bea31f3224d21fe
Shivam <sshivam478@gmail.com>
2022-12-25 17:09:02 +0530

    responsive issue fixed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  resumeLink = 'https://firebasestorage.googleapis.com/v0/b/shivam-singh-e8c32.appspot.com/o/Shivam-Fullstack.pdf?alt=media&token=52ca5614-8dbf-43d5-b94b-195b296872a9';
   constructor() { }
 
   ngOnInit(): void {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      s: '350px',
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1440px'
+    },
   }, plugins: [
     require('tailwind-scrollbar-hide')
     // ...
   ]
-}
+};
